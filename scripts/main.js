61195f900edcd1255a330060af3ade4117259b26
JavascriptFTW <joshuamagma2013@gmail.com>
2015-08-28 17:03:33 -0500

    Redo the entire site
    Redo the entire site with style, and one page-ness. Deleted 2048 folder,
    will have to update gh-pages branch of that repo. UI and navigation
    work, next up is replacing the placeholder text and making the design
    adaptive.

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,332 @@
-function toRadians(angle) {
-  return angle * (Math.PI / 180);
-}
+$(document).ready(function() {
 
-var $rotator = $("#rotator");
-var $rotatorTest = $("#rotator-test");
-var planets = {
-	$els: $(".planet"),
-	sizeFrac: 1 / 7
-};
-planets.radians = toRadians(360 / planets.$els.length);
+	const degRadConst = Math.PI * 2;
 
 
+	var $window = $(window);
 
-var updateRotator = (function updateRotator() {
-	var rSize = Math.min($rotatorTest.width(), $rotatorTest.height());
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();
+	var $body = $(document.body);
 
-	$rotator.width(rSize);
-	$rotator.height(rSize);
-	$rotator.css("left", windowWidth / 2 - rSize / 2);
-	$rotator.css("top", windowHeight / 2 - rSize / 2);
-	planets.$els.width(rSize * planets.sizeFrac);
-	planets.$els.height(rSize * planets.sizeFrac);
+	var $titleContainer = $(".title-container");
 
-	planets.$els.each(function(index) {
-		var $thisEl = $(this);
-		var x = (rSize / 2 + Math.sin(planets.radians * index) * rSize / 2) - rSize * planets.sizeFrac / 2;
-		var y = (rSize / 2 - Math.cos(planets.radians * index) * rSize / 2) - rSize * planets.sizeFrac / 2;
-		$thisEl.css("left", x);
-		$thisEl.css("top", y);
+	var $head = $(".title");
+	var $subhead = $("<p class='title-sub'>Inventor of the potatoe</p>");
+
+	$titleContainer.append($subhead);
+	$body.append($titleContainer);
+
+	var $frameContainer = $(".frame-container");
+
+	var $bubbleContainer = $(".bubble-container");
+
+	var $navControls = $(".nav-bar-controls");
+
+	var $exitFrame = $navControls.find("#exit-frame");
+
+	var $unscrollFrame = $navControls.find("#unscroll-frame");
+
+	$exitFrame.css("opacity", "0");
+	$unscrollFrame.css("opacity", "0");
+
+
+
+	var scrollMomentum = 0;
+
+
+
+	var Bubble = Class.extend({
+		init: function(cfg) {
+			this.$element = $("<div class='bubble'></div>");
+			this.$title = $("<p class='bubble-title'>" + cfg.title + "</p>");
+
+			this.$element.append(this.$title);
+			$bubbleContainer.append(this.$element);
+
+			this.relPos = {
+				x: cfg.x,
+				y: cfg.y,
+				size: cfg.size
+			};
+
+			this.pxPos = {
+				x: null,
+				y: null,
+				size: null
+			};
+
+			this.$element.click(this.onClickWrapper.bind(this));
+
+			this.calcPos();
+
+			this.uid = Date.now();
+		},
+		onClickWrapper: function() {
+			this.onClick();
+		},
+		onClick: function() {
+
+		},
+		calcPos: function() {
+			this.pxPos.size = this.relPos.size / 100 * parseFloat(this.$element.parent().width());
+			this.pxPos.x = this.relPos.x / 100 * parseFloat(this.$element.parent().width()) - this.pxPos.size / 2;
+			this.pxPos.y = 0;
+
+			this.$element.css({
+				"top": this.pxPos.y + "px",
+				"left": this.pxPos.x + "px",
+				"width": this.pxPos.size + "px",
+				"height": this.pxPos.size + "px"
+			});
+			this.$title.css({
+				"top": this.pxPos.size / 2 - parseFloat(this.$title.css("height")) / 2,
+				"left": this.pxPos.size / 2 - parseFloat(this.$title.css("width")) / 2
+			});
+		},
+		update: function() {
+			
+		}
 	});
 
-	return updateRotator;
-})();
 
-var events = {
-	resize: [updateRotator]
-};
 
-$(window).resize(function() {
-	for (var i = 0; i < events.resize.length; i ++) {
-		events.resize[i]();
+	var activeFrame = null;
+
+
+
+	var LinkBubble = Bubble.extend({
+		init: function(cfg) {
+			this._super(cfg);
+
+			this.source = cfg.source;
+
+			this.cosSeed = Math.random() * 360 / degRadConst;
+
+			this.frameAppended = false;
+
+			this.$frameWrapper = $("<div class='frame-wrapper'></div>");
+			this.$frame = $("<iframe class='frame-content'></iframe>");
+
+			this.$frame.attr({
+				/* seamless: "seamless", */
+				frameborder: "0",
+				scrolling: "no",
+				src: this.source
+			})
+
+			this.$frameWrapper.append(this.$frame);
+
+			this.$frameContents = this.$frame.contents().find("html");
+
+			this.wrapperTop = window.innerHeight - parseFloat($frameContainer.css("top")) / 2;
+
+			this.$frameWrapper.css({
+				"top": this.wrapperTop + "px"
+			});
+
+			this.frameScroll = 0;
+
+			this.frameHeight = 0;
+		},
+		drift: function() {
+			this.cosSeed += 0.03;
+			this.cosSeed %= 360;
+			this.$element.css({
+				"top": (this.pxPos.y + Math.cos(this.cosSeed) * this.pxPos.size / 15) + "px"
+			});
+		},
+		onClick: function() {
+			this.$frameWrapper.css("z-index", 1);
+
+			if (activeFrame && activeFrame.uid !== this.uid) {
+				scrollMomentum = 0;
+				activeFrame.deactivate(true);
+			}
+
+			activeFrame = this;
+
+			if (!this.frameAppended) {
+				$frameContainer.append(this.$frameWrapper);
+				this.frameAppended = true;
+			}
+
+			this.$frame.stop().animate({
+				"opacity": "1"
+			}, {
+				duration: 1500
+			});
+			$titleContainer.css("top", "5em");
+
+			// Magicks!
+			this.animateIn();
+
+			$exitFrame.animate({
+				"opacity": 1
+			}, 1000);
+			$exitFrame.css("cursor", "pointer");
+		},
+		animateIn: function() {
+			/* Don't even ask me why this method works. I don't even know, it just works. This scares me */
+			if (parseFloat(this.$frameWrapper.css("top")) > 0) {
+				this.$frameWrapper.css("top", "0px");
+				setTimeout(this.animateIn.bind(this), 1);
+			}
+		},
+		unscroll: function() {
+			setTimeout(this._unscroll.bind(this), 1)
+		},
+		_unscroll: function(times) {
+			scrollMomentum = 0;
+			times = times + 1 || 0;
+			this.frameScroll += 1.2 * times;
+			if (-this.frameScroll > 0) {
+				setTimeout(this._unscroll.bind(this, times), 7)
+			} else {
+				console.log("Shtuff!");
+				this.frameScroll = 0;
+				this.scrolled = false;
+				$unscrollFrame.stop().animate({
+					"opacity": 0
+				}, 1000);
+				$unscrollFrame.css("cursor", "default");
+			}
+		},
+		addScroll: function(amount) {
+			if (this.wrapperTop + this.frameHeight < window.innerHeight) return;
+
+			if (-this.frameScroll >= 500 && $unscrollFrame.css("opacity") === "0") {
+				$unscrollFrame.animate({
+					"opacity": 1
+				}, 1000);
+				$unscrollFrame.css("cursor", "pointer");
+			}
+
+			if (this.frameScroll + amount >= 0) {
+				this.frameScroll = 0;
+			} else if (-(this.frameScroll + amount) > this.frameHeight - 500) {
+				this.frameScroll = -this.frameHeight + 500;
+			} else {
+				this.frameScroll += amount;
+			}
+
+			if (-this.frameScroll > 0) {
+				this.scrolled = true;
+			}
+		},
+		deactivate: function(instant) {
+			instant = instant || false;
+			this.$frameWrapper.css("z-index", -1);
+			this.unscroll();
+			$titleContainer.css("top", "18em");
+
+			setTimeout(this._deactivate.bind(this), 1500 * instant + 1);
+
+			$exitFrame.animate({
+				"opacity": 0
+			}, 1000);
+
+			$unscrollFrame.stop().animate({
+				"opacity": 0
+			}, 1000);
+			$exitFrame.css("cursor", "default");
+		},
+		_deactivate: function() {
+			this.$frame.animate({
+				"opacity": 0
+			}, 1000, function() {
+				this.$frameWrapper.css({
+					"top": window.innerHeight - parseFloat($frameContainer.css("top")) / 2 + "px"
+				});
+			}.bind(this));
+		},
+		update: function() {
+			var frameDocHeight = this.$frame.contents().find("html").height();
+
+			if (this.frameHeight !== frameDocHeight) {
+				this.frameHeight = frameDocHeight;
+				this.$frame.height(frameDocHeight);
+			}
+
+			this.$frame.css("top", this.frameScroll + "px");
+
+			this.drift();
+		}
+	});
+
+
+
+	var bubbles = [
+		new LinkBubble({
+			x: 30,
+			y: 50,
+			size: 5.5 + Math.random() * 2,
+			source: "pages/resume.html",
+			title: "Resume"
+		}),
+		new LinkBubble({
+			x: 40,
+			y: 50,
+			size: 6 + Math.random() * 2,
+			source: "pages/about.html",
+			title: "About Me"
+		}),
+		new LinkBubble({
+			x: 50,
+			y: 50,
+			size: 5 + Math.random() * 2,
+			source: "pages/blog.html",
+			title: "My Blog"
+		}),
+		new LinkBubble({
+			x: 60,
+			y: 50,
+			size: 5.5 + Math.random() * 2,
+			source: "pages/work.html",
+			title: "My Work"
+		}),
+		new LinkBubble({
+			x: 70,
+			y: 50,
+			size: 6.5 + Math.random() * 2,
+			source: "pages/contact.html",
+			title: "Contact Me"
+		})
+	];
+
+
+	$exitFrame.click(function() {
+		scrollMomentum = 0;
+		if (activeFrame) {
+			activeFrame.deactivate();
+		}
+	});
+
+	$unscrollFrame.click(function() {
+		scrollMomentum = 0;
+		if (activeFrame && activeFrame.scrolled) {
+			activeFrame.unscroll();
+		}
+	})
+
+
+
+	function updatePage() {
+		for (var i = 0; i < bubbles.length; i ++) {
+			bubbles[i].update();
+		}
+		if (activeFrame) {
+			activeFrame.addScroll(scrollMomentum);
+			scrollMomentum *= 0.9;
+		}
+
+		setTimeout(updatePage, 10);
 	}
+
+	$body.mousewheel(function(event) {
+		if (activeFrame) {
+			var scrollAmount = event.originalEvent.wheelDelta / 20;
+		    scrollMomentum += scrollAmount;
+		}
+	});
+
+	setTimeout(updatePage, 1)
 });
